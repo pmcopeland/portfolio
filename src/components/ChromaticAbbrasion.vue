416f7a7349eb8457520e1321dc353f1beecd31e5
Paul Copeland <paulmcopeland@gmail.com>
2022-11-20 18:34:23 -0600

    strengthen abrasion on hover

--- src/components/ChromaticAbbrasion.vue
+++ src/components/ChromaticAbbrasion.vue
@@ -1,14 +1,22 @@
 <template>
-        <div class="text chromatic red">{{ text }}</div>
-        <div class="text chromatic green">{{ text }}</div>
-        <div class="text chromatic blue">{{ text }}</div>
+    <div class="chromatic-container" @mouseover="hover = true" @mouseleave="hover = false">
+        {{ hover }}
+        <div class="text chromatic red" :class="{ active: hover }">{{ text }}</div>
+        <div class="text chromatic green" :class="{ active: hover }" >{{ text }}</div>
+        <div class="text chromatic blue" :class="{ active: hover }">{{ text }}</div>
+    </div>
 </template>
 
 <script>
 export default {
     props: {
         text: String
-    }
+    },
+    data() {
+    return {
+      hover: false,
+    };
+  }
 
 }
 </script>
@@ -29,7 +37,7 @@ export default {
 
 .chromatic.red {
     color: #ff0000;
-    left: -5px;
+    left: -2px;
     text-shadow: 0px 0px 5px #ff0000;
 
 }
@@ -42,8 +50,19 @@ export default {
 
 .chromatic.blue {
     color: #0000ff;
-    left: 5px;
+    left: 2px;
     text-shadow: 0px 0px 5px #0000ff;
+}
+
+
+.chromatic.red.active {
+    left: -5px
+}
+.chromatic.green.active {
+    left: 0px
+}
+.chromatic.blue.active {
+    left: 5px
 }
 
 </style>
