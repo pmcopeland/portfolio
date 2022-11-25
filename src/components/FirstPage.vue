50f491c0b21a67c95ed9ef9657f54bfb9379542d
Paul Copeland <paulmcopeland@gmail.com>
2022-11-25 15:36:22 -0600

    minor css/copy changes, fixing font preload

--- src/components/FirstPage.vue
+++ src/components/FirstPage.vue
@@ -2,15 +2,23 @@
 
     <!-- Med and above layout -->
     <MqResponsive target="md+" class="desktop-container">
-            <chromaticAbbrasion :text="this.name"></ChromaticAbbrasion>
-            <div style="background-color: #2C3333; width:33%; height:90%" :v-html="PageText"></div>
+        <chromaticAbbrasion :text="this.name"></ChromaticAbbrasion>
+        <div class="infobox">
+            <h1>Hey there!</h1>
+            <p>
+                In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the
+                visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used
+                as a placeholder before final copy is available. It is also used to temporarily replace text in a
+                process called greeking, which allows designers to consider the form of a webpage or publication,
+                without the meaning of the text influencing the design.</p>
+        </div>
 
     </MqResponsive>
 
     <!-- small layout -->
     <MqResponsive target="sm">
         <chromaticAbbrasion :text="this.name"></ChromaticAbbrasion>
-            <div style="background-color: #2C3333; width:33%; height:90%"> sample div lol</div>
+        <div style="background-color: #2C3333; width:33%; height:90%"> sample div lol</div>
     </MqResponsive>
 </template>
 
@@ -38,18 +46,32 @@ export default {
 body {
     background-color: #181818;
     color: white;
-    height: max-content;
-    
+
+}
+
+.h1 {
+    margin: 5% !important
 }
 
 .desktop-container {
     display: flex;
-    flex:1;
+    flex: 1;
     flex-direction: column;
-    gap:20px;
+    gap: 20px;
     align-items: center;
     justify-content: center;
     height: 100% !important;
 
+}
+
+.infobox {
+    font-family: "Roboto", serif;
+    background-color: #2C3333;
+    width: 33%;
+    min-width: 50rem;
+    height: fit-content;
+    line-height: 1.8;
+    padding: 2rem;
+    border-radius: 15px;
 }
 </style>
